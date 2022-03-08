89901a89edce723594b4f5e462bca66bbaab0beb
micheaol <micheaol80@gmail.com>
2022-03-08 21:37:12 +0000

    Fix linter

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,11 +3,10 @@ class PublicRecipesController < ApplicationController
 
   def index
     @public_recipes = Recipe.all
-    @users = User.all.where("id NOT IN(?)", current_user.id)
+    @users = User.all.where('id NOT IN(?)', current_user.id)
   end
 
   def show
     @public_recipes = Recipe.find(params[:id])
-    
   end
 end
