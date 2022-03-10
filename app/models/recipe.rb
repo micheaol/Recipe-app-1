fe84a0f51eed484b64bd3ff0488248d74fa69668
Juan Carlos <jchernandez827@gmail.com>
2022-03-10 11:58:34 -0500

    Create total and count methods for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,21 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :food_recipes, dependent: :destroy
+
+  def self.count(recipe_id)
+    counter = FoodRecipe.where(recipe_id: recipe_id).count
+    counter
+  end
+
+  def self.total(recipe_id)
+    foods = FoodRecipe.where(recipe_id: recipe_id).includes(:food)
+    total = 0
+    unless foods.empty?
+      foods.each do |food|
+        p food
+        total = total + (food.quantity * food.food.price)
+      end
+    end
+    total
+  end
 end
