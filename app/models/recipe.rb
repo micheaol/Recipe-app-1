dbe9a5b0e187bb1365c26c0b36b8c95688fbfee4
Juan Carlos <jchernandez827@gmail.com>
2022-03-10 12:01:30 -0500

    Linters fix

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,17 +3,15 @@ class Recipe < ApplicationRecord
   has_many :food_recipes, dependent: :destroy
 
   def self.count(recipe_id)
-    counter = FoodRecipe.where(recipe_id: recipe_id).count
-    counter
+    FoodRecipe.where(recipe_id:).count
   end
 
   def self.total(recipe_id)
-    foods = FoodRecipe.where(recipe_id: recipe_id).includes(:food)
+    foods = FoodRecipe.where(recipe_id:).includes(:food)
     total = 0
     unless foods.empty?
       foods.each do |food|
-        p food
-        total = total + (food.quantity * food.food.price)
+        total += (food.quantity * food.food.price)
       end
     end
     total
