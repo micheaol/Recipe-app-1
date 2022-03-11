52d20233b72063ee0fa79d5870064e2d2218b855
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-11 14:51:22 -0500

    View tests - capybara

--- spec/views/food_page_spec.rb
+++ spec/views/food_page_spec.rb
@@ -1,22 +1,46 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-describe 'User index page', type: :feature do
+describe 'User food page', type: :feature do
   before :each do
     user = User.new(name: 'Rafael', email: 'email@test.com', password: '123456')
     user.save
+    
+    Food.create!(name: 'Food 1', measurement_unit: 'Unit', user_id: user.id, price: 1)
 
-    @food = Food.new(name: 'Food 1', measurement_unit: 'Unit', user_id: user.id, price: 3.30)
-    @food.save
+
+    sign_in user
+
 
 
   end
 
-  it 'Trying to access /foods without login' do
+  
+  it 'When I access my food I can see add new food button' do
+    visit '/foods'
+    expect(page).to have_content('Add new Food')
+  end
+  
+  it 'When I access my food I can see my food saved' do
+    visit '/foods'
+    expect(page).to have_content('Food 1')
+  end
+  
+  it 'When I access my recipes page I have <a> tags to see more info about recipes' do
+    visit '/foods'
+    expect(page).to have_css('a')
+  end
+  
+  it 'When I access my food I can see menu button' do
+    visit '/foods'
+    expect(page).to have_content('Menu')
+  end
+  
+end
+
+describe 'User foods page without login', type: :feature do
+  it 'When I access /food without login' do
     visit '/foods'
     expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
-
-
-
 end
