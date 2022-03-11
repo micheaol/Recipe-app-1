a77e076bdad97bc15130b5a6b7c2139962f31aee
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-11 12:27:34 -0500

    Last fixes and tests passed

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -14,17 +14,17 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @current_user = params[:user_id]
-    @new_food = Food.new(user_id: @current_user, name: params[:food][:name],
+    @new_food = Food.new(user_id: current_user.id, name: params[:food][:name],
                          measurement_unit: params[:food][:measurement_unit], price: params[:food][:price])
 
-    if @new_food.save!
+    if @new_food.save
       flash[:notice] = 'Food saved successfully.'
       redirect_to foods_index_path
 
     else
+      p @new_food.errors.full_messages
       flash[:alert] = 'Opps! Something went wrong.'
-      render :new
+      redirect_to foods_new_path
     end
   end
 
